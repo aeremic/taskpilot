8563c5b7cf0f1706f80c5d853b22dae3c6005e69
aeremic <andrija.eremic@gmail.com>
2026-01-03 14:06:24 +0100

    stop

--- deamon/main.go
+++ deamon/main.go
@@ -8,9 +8,11 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
 type processDefinition struct {
+	Pid       int
 	Name      string   `json:"name"`      // "name": "my-api",
 	Cmd       string   `json:"cmd"`       // "cmd": "./my-api",
 	Args      []string `json:"args"`      // "args": ["--port", "8080"],
@@ -21,8 +23,69 @@ type processDefinition struct {
 func write(conn net.Conn, msg string) {
 	_, err := conn.Write([]byte(msg))
 	if err != nil {
-		log.Print(err)
+		log.Fatal(err)
 	}
+}
+
+func startProcess(pd processDefinition) (*exec.Cmd, error) {
+	process := exec.Command(pd.Cmd, pd.Args...)
+	err := process.Start()
+	if err != nil {
+		return nil, err
+	}
+	return process, nil
+}
+
+func getSocketConnection() (net.Listener, error) {
+	socketPath := "/tmp/echo.sock"
+	os.Remove(socketPath)
+
+	l, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return l, nil
+}
+
+func getDbConnection() (*sql.DB, error) {
+	db, err := sql.Open("sqlite", "./storage.db")
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	return db, nil
+}
+
+func reRunProcesses(db *sql.DB) error {
+	var processesFromDb []processDefinition
+	err := db.QueryRow("SELECT * FROM PROCESS;").Scan(&processesFromDb)
+	if err != nil {
+		log.Fatal("Error on database read: ", err)
+	}
+
+	for _, pd := range processesFromDb {
+		processFromSys, err := os.FindProcess(pd.Pid)
+		if err == nil {
+			_, err = startProcess(pd)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		err = processFromSys.Signal(syscall.Signal(0))
+		if err == nil {
+			_, err = startProcess(pd)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+	}
+
+	return nil
 }
 
 func handleConnection(conn net.Conn, db *sql.DB) {
@@ -52,7 +115,7 @@ func handleConnection(conn net.Conn, db *sql.DB) {
 
 			pd, err := common.GetAndDecodeJsonFile[processDefinition](pdPath)
 			process := exec.Command(pd.Cmd, pd.Args...)
-			err = process.Start()
+			process, err = startProcess(*pd)
 			if err != nil {
 				write(conn, err.Error())
 				break
@@ -65,14 +128,38 @@ func handleConnection(conn net.Conn, db *sql.DB) {
 				break
 			}
 
-			_, err = conn.Write([]byte("Process " + pd.Name + " started."))
-			if err != nil {
-				log.Fatal("Error on writing message. ", err)
-			}
+			write(conn, "Process "+pd.Name+" started.")
 			break
 		case "stop":
 			// TODO: Find running processes in the table and get their pids. Then stop them with sigkill. Here it might happen that process is already killed/not existant. Return msg then
 			// TODO: There can be multiple processes by name since multiple processes can be runned so it needs to stop them all
+			if len(msg) < 2 {
+				write(conn, "Invalid start command")
+			}
+
+			var processesFromDb []processDefinition
+			err := db.QueryRow("SELECT * FROM PROCESS WHERE Name = ?;", msg[1]).Scan(&processesFromDb)
+			if err != nil {
+				log.Fatal("Error on database read: ", err)
+			}
+
+			for _, pd := range processesFromDb {
+				process, err := os.FindProcess(pd.Pid)
+				if err != nil {
+					write(conn, err.Error())
+					continue
+				}
+
+				defer process.Release()
+
+				err = process.Signal(syscall.SIGKILL)
+				if err != nil {
+					write(conn, err.Error())
+					continue
+				}
+			}
+
+			write(conn, "Stopping "+msg[1]+" process is done.")
 			break
 		case "restart":
 			// TODO: Find running processes in the table. Sigkill then run them again them if they exist.
@@ -87,30 +174,19 @@ func handleConnection(conn net.Conn, db *sql.DB) {
 }
 
 func main() {
-	socketPath := "/tmp/echo.sock"
-	os.Remove(socketPath)
-
-	l, err := net.Listen("unix", socketPath)
+	l, err := getSocketConnection()
 	if err != nil {
 		log.Fatal("Listen error: ", err)
 	}
 
-	db, err := sql.Open("sqlite", "./storage.db")
+	db, err := getDbConnection()
 	if err != nil {
 		log.Fatal("Database error: ", err)
 	}
-	defer db.Close()
 
-	// TODO: Query processes from the table and run them again if they are not running. Look into syncing states. Rerun ones that are in db but not in sys state
-	var processesFromDb []processDefinition
-	err = db.QueryRow("SELECT * FROM PROCESS;").Scan(&processesFromDb)
+	err = reRunProcesses(db)
 	if err != nil {
-		log.Fatal("Error on database read: ", err)
-	}
-
-	for _, pd := range processesFromDb {
-		process := exec.Command(pd.Cmd, pd.Args...)
-		err = process.Start()
+		log.Fatal("Unable to restart running process: ", err)
 	}
 
 	for {
